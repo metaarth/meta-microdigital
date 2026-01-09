175b1b71793f81efa72a165efd33db5ca9f67fad
metaarth <metaarth@gmail.com>
2026-01-09 18:43:19 +0530

    Add web design image asset and update image source in WebsiteDesign component

--- src/pages/WebsiteDesign.jsx
+++ src/pages/WebsiteDesign.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import PageHeader from '../components/PageHeader'
-import Webdesign from '../Assets/Webimgs/webdesign.jpg'
 
 const WebsiteDesign = () => {
   useEffect(() => {
@@ -38,7 +37,7 @@ const WebsiteDesign = () => {
         <div className="row">
           <div className="col-lg-8">
             <div className="mb-5">
-              <img className="img-fluid w-100" src={Webdesign} alt="Website Design Services" />
+              <img className="img-fluid w-100" src="/assets/images/Webimg/webdesign.jpg" alt="Website Design Services" />
               <h1 className="mt-4 mb-3">Professional Website Design Services</h1>
               <p className="lead">
                 Transform your online presence with our responsive, conversion-focused website design services. We create
